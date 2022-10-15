0a0b6c7b0649adcfb0a191c3eca86eaafb695bb4
SHA43F <shareefpmk44@gmail.com>
2022-10-15 20:41:33 +0530

    ExpenseItems

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,14 @@
+function ExpenseItems(){
+    return (
+        <div>
+            <h1>Expense Items!</h1>
+            <ul>
+                <li>Food 10</li>
+                <li>Petrol 100</li>
+                <li>Movie 200</li>
+            </ul>
+        </div>
+    ) 
+};
+
+export default ExpenseItems;
