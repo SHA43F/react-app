cb96627a2c971c65fd95ea0118367f5fb7358d3f
SHA43F <shareefpmk44@gmail.com>
2022-10-17 11:28:02 +0530

    9.1) Added Location

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,16 +1,42 @@
 import './ExpenseItems.css'
 
-function ExpenseItems(props){
+function ExpenseItems(){
+    const date = new Date(2022, 9, 17);
+    const title = 'ABC';
+    const amount = 400;
+    const location = 'IMAX';
+
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>{date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
-                <div>At {props.location}</div>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{amount}</div>
+                <div>At {location}</div>
             </div>
         </div>
     ) 
 };
 
 export default ExpenseItems;
+
+
+
+
+
+// import './ExpenseItems.css'
+
+// function ExpenseItems(props){
+//     return (
+//         <div className='expense-item'>
+//             <div>{props.date.toISOString()}</div>
+//             <div className='expense-item__description'>
+//                 <h2>{props.title}</h2>
+//                 <div className='expense-item__price'>{props.amount}</div>
+//                 <div>At {props.location}</div>
+//             </div>
+//         </div>
+//     ) 
+// };
+
+// export default ExpenseItems;
