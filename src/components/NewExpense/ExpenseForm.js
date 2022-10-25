acbd897075f42be37ccb6343d2c7331a98b024cb
SHA43F <shareefpmk44@gmail.com>
2022-10-25 12:55:18 +0530

    Update ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,7 +22,7 @@ const ExpenseForm = (props) => {
     e.preventDefault();
     const submitExpenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     props.addNewExpense(submitExpenseData);
